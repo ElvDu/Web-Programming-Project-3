092d7e2d9cbdbaa34d26ac6506bdf0e10cc56678
ElvDu <manidonteven@gmail.com>
2019-04-01 16:58:16 -0400

    product-backend
    -added the backend for:
    -displaying products
    -adding products to cart
    
    -also some minor styling

--- purchase.php
+++ purchase.php
@@ -1,11 +1,24 @@
 <?php
 	session_start();
 	require "config.php";
-	
-	$sample = mysqli_query($link, "SELECT * FROM users WHERE id = 1");
-	
-	while($row = $sample->fetch_assoc()){
-		echo $row['email']."<br>";
+	if(isset($_GET['action']) && $_GET['action']=="add"){
+		$id=intval($_GET['id']);
+		
+		if(isset($_SESSION['cart'][$id])){
+			$_SESSION['cart'][$id]['quantity']++;
+		}else{
+			$query_s=mysqli_query($link, "SELECT * FROM product WHERE id={$id}");
+			if(mysqli_num_rows($query_s)!=0){
+				$row_s=mysqli_fetch_array($query_s);
+				
+				$_SESSION['cart'][$row_s['id']]=array(
+					"quantity"=>1,
+					"price"=>$row_s['COST']
+				);
+			}else{
+				$message=mysqli_error($link);
+			}
+		}
 	}
 ?>
 <!DOCTYPE html>
@@ -23,6 +36,11 @@
 		</div>
 		<div id="motto">
 			<h1>This is Product of FISH</h1>
+			<?php
+				if(isset($message)){
+					echo "<h2>$message</h2>";
+				}
+				?>
 			F I S H
 		</div>
 	</div>
@@ -45,8 +63,9 @@
 			<li><a href="cart.php">Cart</a></li>
 		</ul>
 	</nav>
-	<h2>Please choose from our four categories of products, or select from all items.</h2>
+	
 	<div id="categories">
+		<h2>Please choose from our four categories of products, or select from all items.</h2>
 		<ul>
 			<li><a href="">Fish</a></li>
 			<li><a href="">Food</a></li>
@@ -55,23 +74,22 @@
 		</ul>
 	</div>
 	<?php
-		$product_array = mysqli_query($link, "SELECT * FROM product ORDER BY id ASC");
-		if (!empty($product_array)){
-			foreach($product_array as $key=>$value){
+		$product_list = mysqli_query($link, "SELECT * FROM product ORDER BY id ASC");
+	
+	while($row = $product_list->fetch_assoc()){
 	?>
 	<div class="product">
-		<form method="post" action="purchase.php?action=add&code=<?php echo $product_array[$key]["code"]; ?>">
-		<div class="product-image"><img src="<?php echo $product_array[$key]["image_url"]; ?>"></div>
+		<form method="post" action="purchase.php?action=add&id=<?php echo $row['id']; ?>">
+		<div class="product-image"><img src="<?php echo $row['image_url']; ?>"></div>
 		<div class="product-tile-footer">
-			<div class="product-title"><?php echo $product_array[$key]["PRODUCT_NAME"]; ?></div>
-			<div class="product-price"><?php echo "$".$product_array[$key]["COST"]; ?></div>
+			<div class="product-title"><?php echo $row['PRODUCT_NAME']; ?></div>
+			<div class="product-price"><?php echo "$".$row['COST']; ?></div>
 			<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
 		</div>
 		</form>
 	</div>
 <?php
 	}
-}
 ?>
 	
 </body>
