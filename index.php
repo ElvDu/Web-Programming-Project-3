f84a6967a655527bbbe9e46ff85a2d57c5e48f2c
ElvDu <manidonteven@gmail.com>
2019-04-01 20:32:13 -0400

    cart-functionality
    -added cart functionality

--- index.php
+++ index.php
@@ -45,78 +45,77 @@ switch($_GET["action"]) {
 }
 }
 ?>
-<!DOCTYPE html>
-<html lang="en">
+<HTML>
+<HEAD>
+<TITLE>Simple PHP Shopping Cart</TITLE>
+<link href="style.css" type="text/css" rel="stylesheet" />
+</HEAD>
+<BODY>
+<div id="shopping-cart">
+<div class="txt-heading">Shopping Cart</div>
 
-<head>
-	<link rel="stylesheet" type="text/css" href="index.css">
-	<title>Product of FISH</title>
-</head>
-
-<body>
-	<div id="logoandmotto">
-		<div id="logo">
-			<img src="./images/fesh.png">
-		</div>
-		<div id="motto">
-			<h1>This is Product of FISH</h1>
-			<?php
-				if(isset($message)){
-					echo "<h2>$message</h2>";
-				}
-				?>
-			F I S H
-		</div>
-	</div>
-	<div id="login">
-		<a href="signup.html">sign up</a>
-		<a href="login.html">login</a>
-	</div>
-	
-	<nav id="navbar">
-		<ul>
-			<li><a href="index.html">Home</a></li>
-			<li><a class="current-page" href="purchase.php">Purchase</a>
-				<ul>
-					<li><a href="fish.php">Fish</a></li>
-					<li><a href="">Food</a></li>
-					<li><a href="">Homes</a></li>
-					<li><a href="">Accessories</a></li>
-				</ul>
-			</li>
-			<li><a href="checkout.php">Cart<?php 
-			$total_quantity = 0;
-			if(!empty($_SESSION['cart_item'])){
-				foreach($_SESSION['cart_item'] as $item){
+<a id="btnEmpty" href="index.php?action=empty">Empty Cart</a>
+<?php
+if(isset($_SESSION["cart_item"])){
+    $total_quantity = 0;
+    $total_price = 0;
+?>	
+<table class="tbl-cart" cellpadding="10" cellspacing="1">
+<tbody>
+<tr>
+<th style="text-align:left;">Name</th>
+<th style="text-align:right;" width="5%">Quantity</th>
+<th style="text-align:right;" width="10%">Unit Price</th>
+<th style="text-align:right;" width="10%">Price</th>
+<th style="text-align:center;" width="5%">Remove</th>
+</tr>	
+<?php		
+    foreach ($_SESSION["cart_item"] as $item){
+        $item_price = $item["quantity"]*$item["price"];
+		?>
+				<tr>
+				<td><img src="<?php echo $item["image"]; ?>" class="cart-item-image" /><?php echo $item["name"]; ?></td>
+				<td><?php echo $item["id"]; ?></td>
+				<td style="text-align:right;"><?php echo $item["quantity"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ".$item["price"]; ?></td>
+				<td  style="text-align:right;"><?php echo "$ ". number_format($item_price,2); ?></td>
+				<td style="text-align:center;"><a href="index.php?action=remove&id=<?php echo $item["id"]; ?>" class="btnRemoveAction"><img src="icon-delete.png" alt="Remove Item" /></a></td>
+				</tr>
+				<?php
 				$total_quantity += $item["quantity"];
-				}
-			}
-				echo '('.$total_quantity.')';?></a></li>
-		</ul>
-	</nav>
-	
-	<div id="categories">
-		<h2>Please choose from our four categories of products, or select from all items.</h2>
-		<ul>
-			<li><a href="fish.php">Fish</a></li>
-			<li><a href="">Food</a></li>
-			<li><a href="">Homes</a></li>
-			<li><a href="">Accessories</a></li>
-		</ul>
-	</div>
-	
-	<div id="product-grid">
+				$total_price += ($item["price"]*$item["quantity"]);
+		}
+		?>
+
+<tr>
+<td colspan="2" align="right">Total:</td>
+<td align="right"><?php echo $total_quantity; ?></td>
+<td align="right" colspan="2"><strong><?php echo "$ ".number_format($total_price, 2); ?></strong></td>
+<td></td>
+</tr>
+</tbody>
+</table>		
+  <?php
+} else {
+?>
+<div class="no-records">Your Cart is Empty</div>
+<?php 
+}
+?>
+</div>
+
+<div id="product-grid">
+	<div class="txt-heading">Products</div>
 	<?php
 	$product_array = $db_handle->runQuery("SELECT * FROM product ORDER BY id ASC");
 	if (!empty($product_array)) { 
 		foreach($product_array as $key=>$value){
 	?>
-		<div class="product">
-			<form method="post" action="purchase.php?action=add&id=<?php echo $product_array[$key]["id"]; ?>">
+		<div class="product-item">
+			<form method="post" action="index.php?action=add&id=<?php echo $product_array[$key]["id"]; ?>">
 			<div class="product-image"><img src="<?php echo $product_array[$key]["image_url"]; ?>"></div>
 			<div class="product-tile-footer">
 			<div class="product-title"><?php echo $product_array[$key]["PRODUCT_NAME"]; ?></div>
-			<div class="product-description"><?php echo '"'.$product_array[$key]['DESCRIPTION'].'"'; ?></div>
 			<div class="product-price"><?php echo "$".$product_array[$key]["COST"]; ?></div>
 			<div class="cart-action"><input type="text" class="product-quantity" name="quantity" value="1" size="2" /><input type="submit" value="Add to Cart" class="btnAddAction" /></div>
 			</div>
@@ -127,5 +126,5 @@ switch($_GET["action"]) {
 	}
 	?>
 </div>
-</body>
-</html>
+</BODY>
+</HTML>
